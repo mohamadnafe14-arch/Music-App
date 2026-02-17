c34530908b63242b79915d5729e325d73e6f6d22
mohamed <mnafe44@gmail.com>
2026-02-17 12:36:17 +0200

    adding auth button

---
+++ client/lib/features/auth/view/widgets/auth_button.dart
@@ -0,0 +1,42 @@
+import 'package:client/core/theme/app_palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AuthButton extends StatelessWidget {
+  const AuthButton({
+    super.key,
+    required this.onPressed,
+    required this.text,
+    required this.isLoading,
+  });
+  final VoidCallback onPressed;
+  final String text;
+  final bool isLoading;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [AppPalette.gradient1, AppPalette.gradient2],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          fixedSize: Size(300.w, 50.h),
+        ),
+        onPressed: onPressed,
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : Text(
+                text,
+                style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500),
+              ),
+      ),
+    );
+  }
+}
