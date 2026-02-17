c4891187445eb3a804ed33a447387905f7d7a99f
mohamed <mnafe44@gmail.com>
2026-02-17 12:20:01 +0200

    adding auth text form field

--- client/lib/core/theme/app_theme.dart
+++ client/lib/core/theme/app_theme.dart
@@ -1,8 +1,22 @@
 import 'package:client/core/theme/app_palette.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTheme {
-  static final darkMode = ThemeData.dark().copyWith(
+  static final appTheme = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPalette.backgroundColor,
+    appBarTheme: AppBarTheme(backgroundColor: AppPalette.backgroundColor),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.r)),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPalette.gradient2),
+      errorBorder: _border(AppPalette.errorColor),
+      contentPadding: EdgeInsets.all(16.h),
+    ),
   );
+  static InputBorder _border([Color color = AppPalette.borderColor]) =>
+      OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.r),
+        borderSide: BorderSide(color: color, width: 3.w),
+      );
 }
