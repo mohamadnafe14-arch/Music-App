360420efe6b7f0cd53b572812f489087d303c02d
mohamed <mnafe44@gmail.com>
2026-02-18 11:47:39 +0200

    linking the dataabase into the project

--- server/main.py
+++ server/main.py
@@ -1,6 +1,12 @@
 from fastapi  import FastAPI, Request 
 from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 app = FastAPI()
+DATABASE_URL = "postgresql://postgres:medo2005#@localhost:5432/postgres5432/music_app"
+engine = create_engine(DATABASE_URL)
+sessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db=sessionLocal()
 class UserCreate(BaseModel):
     name: str
     email: str
