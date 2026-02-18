ef2c731f84eb5525ac73cd007cf70a227a4c25f4
mohamed <mnafe44@gmail.com>
2026-02-18 08:17:47 +0200

    installing fasr api and user create class

--- server/main.py
+++ server/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from fastapi  import FastAPI, Request 
+from pydantic import BaseModel
+app = FastAPI()
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+@app.post("/")
+async def root(user: UserCreate):
+    return "The email of the user is: " + user.email + " and the password is: " + user.password + " and the name is: " + user.name
