eda4cddcefd9605f958863bcd07f06d24822d54f
mohamed <mnafe44@gmail.com>
2026-02-18 13:43:41 +0200

    refactoring the file seperation

--- server/database.py
+++ server/database.py
@@ -4,4 +4,9 @@ from models.base import Base
 DATABASE_URL = "postgresql://postgres:medo2005%23@localhost:5432/music_app"
 engine = create_engine(DATABASE_URL)
 sessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
-db=sessionLocal()
+def get_db():
+    db=sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,15 +1,16 @@
 import uuid
 import bcrypt
-from fastapi import APIRouter, HTTPException
-from user_create import UserCreate
-from database import db
+from fastapi import APIRouter, Depends, HTTPException
+from database import get_db
+from routes.pydantic_schemas.user_create import UserCreate
 from models.user import User
+from sqlalchemy.orm import Session
 router=APIRouter()
 @router.post("/signup")
-async def root(user: UserCreate):
+async def signup(user: UserCreate,db: Session = Depends(get_db)):
   user_db = db.query(User).filter(User.email==user.email).first()
   if   user_db:
-    raise HTTPException(400,"User with this email does not exist")
+    raise HTTPException(400,"User with this email already exist")
   hased_password=bcrypt.hashpw(user.password.encode(),bcrypt.gensalt())
   user_db=User(id=str(uuid.uuid4()),name=user.name,email=user.email,password=hased_password)
   db.add(user_db)
