3bada19803d6e6abc6aef2aff5cf86a9200fa78e
mohamed <mnafe44@gmail.com>
2026-02-20 11:10:01 +0200

    Adding riverpod of remote auth repo

--- client/lib/features/auth/repos/remote_auth_repo.dart
+++ client/lib/features/auth/repos/remote_auth_repo.dart
@@ -5,7 +5,10 @@ import 'package:client/core/errors/failure.dart';
 import 'package:client/features/auth/model/user.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'remote_auth_repo.g.dart';
+@riverpod
+RemoteAuthRepo remoteAuthRepo(RemoteAuthRepoRef ref) => RemoteAuthRepo();
 class RemoteAuthRepo {
   Future<Either<Failure, User>> signIn(String email, String password) async {
     final response = await http.post(
---
+++ client/lib/features/auth/repos/remote_auth_repo.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'remote_auth_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$remoteAuthRepoHash() => r'741292f0e7f9b6a6b33ae5dce43c9403f8eb5431';
+
+/// See also [remoteAuthRepo].
+@ProviderFor(remoteAuthRepo)
+final remoteAuthRepoProvider = AutoDisposeProvider<RemoteAuthRepo>.internal(
+  remoteAuthRepo,
+  name: r'remoteAuthRepoProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$remoteAuthRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+// ignore: deprecated_member_use
+typedef RemoteAuthRepoRef = AutoDisposeProviderRef<RemoteAuthRepo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
