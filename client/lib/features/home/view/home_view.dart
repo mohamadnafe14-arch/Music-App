891b91d3c0d708c427c2efbf109018edf1ab76c7
mohamed <mnafe44@gmail.com>
2026-02-21 14:34:58 +0200

    adding home body

--- client/lib/features/home/view/home_view.dart
+++ client/lib/features/home/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:client/features/home/view/widgets/home_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Scaffold(body: HomeBody()),
+    );
   }
 }
